674f7f89484a9b4daebbca76a62ebf618105de39
Daniel Muszkiet <danielmuszkiet.marketing@gmail.com>
2025-05-26 08:40:49 +0200

    Update fetch URL to use HTTPS for movie details

--- src/App.jsx
+++ src/App.jsx
@@ -251,7 +251,7 @@ function MovieDetails({ selectedID, onCloseMovie, onAddWatched, watched }) {
   useEffect(() => {
     async function getMovieDetails() {
       setIsLoading(true);
-      const res = await fetch(`http://www.omdbapi.com/?apikey=${KEY}&i=${selectedID}`);
+      const res = await fetch(`https://www.omdbapi.com/?apikey=${KEY}&i=${selectedID}`);
       const data = await res.json();
       setMovie(data);
       setIsLoading(false);
